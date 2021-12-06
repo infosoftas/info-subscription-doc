1850267acc8118e221acebd3448299d7c49513eb
Esben Bach <esben@ofn.dk>
2021-12-06 15:58:33 +0100

    Changed config for TOC rendering

--- source/conf.py
+++ source/conf.py
@@ -89,6 +89,8 @@ html_theme = 'sphinx_rtd_theme'
 #
 html_theme_options = {
     'navigation_depth' : 2,
+    'collapse_navigation' : False,
+    'sticky_navigation' : False
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
